36bbda39aa1b715d2f73bba9bbe3aac8eef0eb5f
Gevorg <g.danelyan@worksolutions.ru>
2022-09-07 09:34:50 +0300

    Добавлены замечания

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+// FIXME перепиши все на ts) для запуска можно заюзать nodemon
+// FIXME все импорты и экспорты через import/export ts-овский
 const Server = require('./server');
 const Controller = require('./controller');
 const routes = require('./routes');
 
+// FIXME странно видеть process.env.PORT, когда в проекте нет .env.example
 const PORT = process.env.PORT || 3000
 
 const server = new Server({
+	// FIXME хочу два контроллера подключить, как быть?
 	controller: new Controller(routes),
 	port: PORT,
 }).start(() => {
--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,16 @@
 const http = require("http");
-
+// FIXME сделать Server - сервисом при помощи typedi
+// FIXME @Service()
 class Server {
 	constructor({
 		controller,
 		port,
 	            }) {
-		this.controller = controller;
-		this.port = port;
+		this.controller = controller; // FIXME private in constructor
+		this.port = port; // FIXME private in constructor
 
 		this.server = http.createServer((req, res) => {
+			// FIXME контролеров должно быть несколько
 			this.controller.handle(req,res)
 		})
 	}
